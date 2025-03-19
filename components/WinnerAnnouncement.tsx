cb53fac972050a42d01ef288022fc9c4909e67d5
Sailesh Sivakumar <32437884+ss251@users.noreply.github.com>
2025-03-19 16:47:37 -0500

    update tweet embed for latest winner

--- components/WinnerAnnouncement.tsx
+++ components/WinnerAnnouncement.tsx
@@ -21,7 +21,7 @@ const ANNOUNCEMENT_TWEETS: Record<number, string> = {
   11: "https://x.com/qrcoindotfun/status/1901352755903738331",
   12: "https://x.com/qrcoindotfun/status/1901688919155999219",
   13: "https://x.com/qrcoindotfun/status/1902062098102050835",
-  14: "https://x.com/qrcoindotfun/status/1902474553420607528"
+  14: "https://x.com/qrcoindotfun/status/1902476625503252928"
 };
 
 export function WinnerAnnouncement({ auctionId }: WinnerAnnouncementProps) {
