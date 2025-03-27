63405e8958e2e4f0e0b49a2aa5226f25233397d0
Sailesh Sivakumar <32437884+ss251@users.noreply.github.com>
2025-03-27 12:30:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@
 export default function HomePage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">Under Construction - brb</h1>
+      <h1 className="text-4xl font-bold">the website is currently under construction. please come back later.</h1>
     </div>
   );
 }
