e21c8186ca7bdf21f4dfdd0fbe1aa5c3317d5c57
ss251 <ss251@uw.edu>
2025-03-29 12:00:58 +0530

    feat: disable auto-connect in farcaster mini app

--- providers/FrameProvider.tsx
+++ providers/FrameProvider.tsx
@@ -1,20 +1,13 @@
 "use client";
 import FrameSDK from "@farcaster/frame-sdk";
-import farcasterFrame from "@farcaster/frame-wagmi-connector";
 import type { ReactNode } from "react";
-import { customconfig } from "@/config/config";
-import { connect } from "wagmi/actions";
 import { useEffect } from "react";
 
 export function FarcasterFrameProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const init = async () => {
-      const context = await FrameSDK.context;
-
-      // Autoconnect if running in a frame.
-      if (context?.client.clientFid) {
-        connect(customconfig, { connector: farcasterFrame() });
-      }
+      // Get context but don't auto-connect
+      await FrameSDK.context;
 
       // Hide splash screen after UI renders.
       setTimeout(() => {
