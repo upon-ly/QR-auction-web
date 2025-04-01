bac697e6a539e99a6c144c79b79a722906622e80
ss251 <ss251@uw.edu>
2025-04-01 20:50:41 +0530

    fix: $QR download file name

--- components/QRContextMenu.tsx
+++ components/QRContextMenu.tsx
@@ -78,8 +78,8 @@ export function QRContextMenu({ children, className, isHeaderLogo = false }: QRC
 
   const handleDownloadQR = () => {
     const link = document.createElement("a");
-    link.href = "/qrLogo.png";
-    link.download = encodeURIComponent("$QR.png");
+    link.href = "/$QR.png";
+    link.download = "$QR.png";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
