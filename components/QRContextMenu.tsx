1d6c9b7e0ecf0aaeb7fbc4783e7a219b5080bba2
ss251 <ss251@uw.edu>
2025-04-01 17:48:31 +0530

    feat: change file name for qr logo download

--- components/QRContextMenu.tsx
+++ components/QRContextMenu.tsx
@@ -79,7 +79,7 @@ export function QRContextMenu({ children, className, isHeaderLogo = false }: QRC
   const handleDownloadQR = () => {
     const link = document.createElement("a");
     link.href = "/qrLogo.png";
-    link.download = "qrLogo.png";
+    link.download = "$QR.png";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
