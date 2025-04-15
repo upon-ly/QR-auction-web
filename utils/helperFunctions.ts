f7cc7ae6a3ec15d58069eda9cceb5b4c344dacf6
ss251 <ss251@uw.edu>
2025-04-16 05:10:31 +0530

    fix: format URLs

--- utils/helperFunctions.ts
+++ utils/helperFunctions.ts
@@ -24,7 +24,7 @@ export function formatURL(url: string, today = false, mobile = false) {
     }
     
     // Determine maximum path length based on today parameter
-    const maxPathLength = today ? mobile ? 18 : 18 : 15;
+    const maxPathLength = today ? mobile ? 33 : 34 : 21;
     
     // If the path is short enough to display fully, don't add ellipsis
     if (path.length <= maxPathLength) {
