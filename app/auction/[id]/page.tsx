a1944e39939de14d6dcd1cc08ea6ddf080c595f4
ss251 <ss251@uw.edu>
2025-05-15 19:49:10 +0530

    feat: update latest winner image

--- app/auction/[id]/page.tsx
+++ app/auction/[id]/page.tsx
@@ -351,7 +351,8 @@ export default function AuctionPage() {
         64: "https://i.postimg.cc/KzBYyrMy/63winner.jpg",
         65: "https://i.postimg.cc/kMyLJhFj/64winner.jpg",
         66: "https://i.postimg.cc/wTDHNwnp/43winner.jpg",
-        68: "https://i.postimg.cc/3Jmz8MzD/67winner.jpg"
+        68: "https://i.postimg.cc/3Jmz8MzD/67winner.jpg",
+        71: "https://i.postimg.cc/0Nh72ypw/70winner.jpg"
     }),
     []
   );
--- components/WinDetailsView.tsx
+++ components/WinDetailsView.tsx
@@ -170,7 +170,8 @@ export function WinDetailsView(winnerdata: AuctionType) {
           "63": "https://i.postimg.cc/KzBYyrMy/63winner.jpg",
           "64": "https://i.postimg.cc/kMyLJhFj/64winner.jpg",
           "65": "https://i.postimg.cc/wTDHNwnp/43winner.jpg",
-          "67": "https://i.postimg.cc/3Jmz8MzD/67winner.jpg"
+          "67": "https://i.postimg.cc/3Jmz8MzD/67winner.jpg",
+          "70": "https://i.postimg.cc/0Nh72ypw/70winner.jpg"
         };
 
         // Check if we have a custom image override for this auction
