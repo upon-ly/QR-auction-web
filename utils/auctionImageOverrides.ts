c16565113410e399c1f0dca58a5d9510aae11465
ss251 <ss251@uw.edu>
2025-05-24 20:08:25 +0530

    feat: update og image for latest winner

--- utils/auctionImageOverrides.ts
+++ utils/auctionImageOverrides.ts
@@ -49,7 +49,6 @@ export const auctionImageOverrides: Record<string, string> = {
   "76": "https://i.postimg.cc/85DwR5m5/74winner.jpg",
   "77": "https://gwum763zx9.ufs.sh/f/xTE4HUVGCg4nExWMi4v83tMP5l2AzXYsrIBd7hNc1eLqZuCG",
   "78": "https://i.postimg.cc/qRLKMsk0/78winner.jpg",
-  "79": "https://i.postimg.cc/qRLKMsk0/78winner.jpg"
 };
 
 /**
