1e3265dcaacaef7082499c519ab849f01ee05923
ss251 <ss251@uw.edu>
2025-05-30 22:09:23 +0530

    feat: add quote tweet as website embed

--- components/LinkVisitClaimPopup.tsx
+++ components/LinkVisitClaimPopup.tsx
@@ -383,7 +383,7 @@ export function LinkVisitClaimPopup({
       const shareText = encodeURIComponent(`just got paid 1,000 $QR to check out today's winner @qrcoindotfun`);
       
       // TODO: Replace this with the actual tweet URL you want to quote
-      const tweetToQuote = "";
+      const tweetToQuote = "https://qrcoin.fun";
       
       const shareUrl = `https://twitter.com/intent/tweet?text=${shareText}&url=${encodeURIComponent(tweetToQuote)}`;
       
