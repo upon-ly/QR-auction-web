a506f5842e6a2c3cbcca3c9fbbd6a9b1671c8125
FTCHD <144691102+FTCHD@users.noreply.github.com>
2025-07-05 20:04:18 +0100

    update delete route (#86)

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -9,16 +9,14 @@ export async function GET() {
   await supabase
     .from('link_visit_claims')
     .delete()
-    // .filter('claimed_at', 'gt', 'CURRENT_DATE - 1') should be the same as below
-    .gt('claimed_at', 'CURRENT_DATE - INTERVAL \'1 day\'')
+    .gt('claimed_at', new Date(new Date().setDate(new Date().getDate() - 1)).toISOString())
     .or(`user_id.eq.did:privy:cmbjsk07z01h8l40meax6d101,fid.eq.1020,eth_address.eq.0xE08D1B1D3800BeBD1ecAc887304fd53A283334aA,eth_address.eq.0x363E8f2E9e6A901bC6630387BecDEb3508D390DE,user_id.eq.did:privy:cmacm8oks02j6js0mxlmqfvbh,user_id.eq.did:privy:cmcjm1gti01lcjy0loymqvqm5`);
 
 		
   await supabase
     .from('welcome_claims')
     .delete()
-    // .filter('created_at', 'gt', 'CURRENT_DATE - 1') should be the same as below
-    .gt('created_at', 'CURRENT_DATE - INTERVAL \'1 day\'')
+    .gt('claimed_at', new Date(new Date().setDate(new Date().getDate() - 1)).toISOString())
     .or(`user_id.eq.did:privy:cmbjsk07z01h8l40meax6d101,user_id.eq.did:privy:cmacm8oks02j6js0mxlmqfvbh,user_id.eq.did:privy:cmcjm1gti01lcjy0loymqvqm5`);
     
   return new Response('Success');
